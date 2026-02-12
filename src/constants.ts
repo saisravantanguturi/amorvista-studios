6d284746410cc89058d34ad6f603bb24809d6173
Sai Sravan <saisravan7.t@gmail.com>
2026-02-12 18:55:44 +0530

    Updated Sai Sravan images

--- src/constants.ts
+++ src/constants.ts
@@ -13,7 +13,7 @@ export const TEAM_MEMBERS: TeamMember[] = [
     role: "Founder", 
     specialties: ["Photography", "Videography", "Editing"],
     // Base image for the card background
-    image: "https://img.sanishtech.com/u/5ad67b75217c006cb34f685078ceebcb.jpg",
+    image: "/sravan_back.jpg",
     // Transparent cutout for the 3D pop-out effect
     characterImage: "https://img.sanishtech.com/u/ad5d47a78c4238b5c5a0cb5a558954e8.png"
   },
