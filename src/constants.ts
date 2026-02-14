eafe40a93ee793d9e566d8d7635c800108014aa3
Sai Sravan <saisravan7.t@gmail.com>
2026-02-14 23:57:28 +0530

    Forced desktop view for entire site on mobile

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,12 @@
 import { TeamMember, Service, Testimonial, Album, ImageSlide } from './types';
 
 export const HERO_IMAGES: ImageSlide[] = [
-  { url: "/hero_1.jpg", alt: "Wedding Shoot" },  // <-- Change these to your actual file names!
-  { url: "/s6.JPG", alt: "Cultural Event" },
-  { url: "/hero_2.jpg", alt: "Model Shoot" },
+  { url: "/s6.JPG", alt: "Wedding Shoot" },  // <-- Change these to your actual file names!
+  { url: "/h3.jpeg", alt: "Cultural Event" },  // hero_1.jpg
+  { url: "/h1.jpeg", alt: "Model Shoot" },   // hero_2.jpg
   { url: "/hero_6.jpg", alt: "Corporate Event" },
   { url: "/hero_3.jpg", alt: "Model Shoot" },
-  { url: "/naa_018.jpg", alt: "Corporate Event" },
+  { url: "/h2.jpeg", alt: "Corporate Event" },
 ];
 
 export const TEAM_MEMBERS: TeamMember[] = [
